6d9857b7bd1b6bb2574caf976829393e6691a33a
fear <gerasimovsawwa@gmail.com>
2025-04-04 20:42:34 +0400

    fixed pixel ratio v.3

--- map_move_zoom/js/threeScene2.js
+++ map_move_zoom/js/threeScene2.js
@@ -93,8 +93,8 @@ function initThreeScene() {
 
     // Ограничиваем диапазон размеров, например, 200 - 600
     // Получаем реальные размеры экрана без учета полосы прокрутки
-    const width = screen.width*window.devicePixelRatio;
-    const height = screen.height*window.devicePixelRatio;  
+    const width = screen.width/window.devicePixelRatio;
+    const height = screen.height/window.devicePixelRatio;  
 
     
     meshSize = Math.min(Math.max(Math.max(width,height)*0.35, 360),440);
