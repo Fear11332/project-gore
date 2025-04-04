2d2ee7f9ed115dd50617887da46fb14ffecf8ed4
fear <gerasimovsawwa@gmail.com>
2025-04-04 17:57:23 +0400

    size targetting

--- map_move_zoom/js/threeScene2.js
+++ map_move_zoom/js/threeScene2.js
@@ -99,9 +99,9 @@ function initThreeScene() {
     meshSize = Math.min(Math.max(Math.max(width,height)*0.35, 350),666);
     camera = new THREE.PerspectiveCamera(75,1, 1, 10000); // aspect = 1
     console.log("mesh size", meshSize);
-    console.log("30%",Math.max(width,height)*0.3);
+    console.log("%",Math.max(width,height)*0.35);
     console.log("width height",width, height);
-    console.log("current size", Math.min(Math.max(Math.max(width,height)*0.3, 330),666));
+    console.log("current size", Math.min(Math.max(Math.max(width,height)*0.35, 350),666));
      
     renderer.setSize(meshSize,meshSize);
     renderer.setPixelRatio(window.devicePixelRatio);
