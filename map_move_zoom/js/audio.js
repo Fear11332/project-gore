79ebafaed98df92a264085bcf9428b9a05343c7e
fear <gerasimovsawwa@gmail.com>
2025-04-17 03:05:59 +0400

    added custome audio player

---
+++ map_move_zoom/js/audio.js
@@ -0,0 +1,18 @@
+  const audio = document.getElementById('bg-music');
+  const toggleBtn = document.getElementById('mute-toggle');
+
+  // Стартуем с muted
+  audio.muted = true;
+
+  toggleBtn.addEventListener('click', () => {
+    // Если первый запуск — включаем звук и запускаем музыку
+    if (audio.paused) {
+      audio.muted = false;
+      audio.play();
+      toggleBtn.textContent = '🔊';
+    } else {
+      // Переключаем mute
+      audio.muted = !audio.muted;
+      toggleBtn.textContent = audio.muted ? '🔇' : '🔊';
+    }
+  });
