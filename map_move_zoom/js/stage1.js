2775ed09e10234c34bfb15e63cbc141f257b2a0b
savva <gerasimovsawwa@gmail.com>
2025-06-06 03:51:59 +0400

    added clouds and animations and text field enter to map

--- map_move_zoom/js/stage1.js
+++ map_move_zoom/js/stage1.js
@@ -16,6 +16,7 @@ const config = {
 const game = new Phaser.Game(config);
 let showStage2 = false;
 const stage1 = document.getElementById('stage1');
+let stage  = 'stage0';
 
 function preload() { 
     this.load.image('cross','https://fear11332.github.io/project-gore/map_move_zoom/images/goreme_site_stage1_cross_1_02.webp');
@@ -23,9 +24,15 @@ function preload() {
     this.load.image('a1', 'https://fear11332.github.io/project-gore/map_move_zoom/images/goreme_site_stage1_tile_A1_1_01.webp');
     this.load.image('a3', 'https://fear11332.github.io/project-gore/map_move_zoom/images/goreme_site_stage1_tile_A3_1_02.webp');
     this.load.image('a4', 'https://fear11332.github.io/project-gore/map_move_zoom/images/goreme_site_stage1_tile_A4_1_02.webp');
+    this.load.image('cloud1', 'https://fear11332.github.io/project-gore/map_move_zoom/images/goreme_site_st0_clouds_1_02.webp');
+    this.load.image('cloud2', 'https://fear11332.github.io/project-gore/map_move_zoom/images/goreme_site_st0_clouds_2_02.webp');
+    this.load.image('cloud3', 'https://fear11332.github.io/project-gore/map_move_zoom/images/goreme_site_st0_clouds_3_02.webp');
+    this.load.image('cloud4', 'https://fear11332.github.io/project-gore/map_move_zoom/images/goreme_site_st0_clouds_4_02.webp');
+    this.load.image('enter_to_stage1', 'https://fear11332.github.io/project-gore/map_move_zoom/images/goreme_site_stage1_text_1_02.webp');
 }
 
 function create() {
+    this.isTransitioning = false;
     const texture = this.textures.get('cross').getSourceImage();
     const originalWidth = texture.width;
     const originalHeight = texture.height;
@@ -37,6 +44,29 @@ function create() {
     const scaleY = screenH / originalHeight;
     const scale = Math.max(scaleX, scaleY);
 
+    this.cloudLayers = [];
+
+  // Создаем контейнеры для облаков
+  for (let i = 0; i < 4; i++) {
+    let container = this.add.container(0, 0);
+    // Добавляем облако (здесь просто placeholder)
+    container.add(this.add.image(0, 0, `cloud${i+1}`).setOrigin(0.5, 0.5).setScale(scale));
+    container.setPosition(screenW / 2, screenH / 2); // Центрируем контейнер
+    this.cloudLayers.push(container);
+  }
+
+
+  this.cloudLayers.forEach(layer => layer.setDepth(100));
+
+  this.enterToStage1 = this.add.image(screenW / 2, screenH / 2, 'enter_to_stage1')
+        .setOrigin(0.5)
+        .setScale(scale)
+        .setDepth(200);  // повыше облаков (у них 100)
+
+  // Скорости для каждого слоя (чем дальше — тем медленнее)
+  this.cloudSpeeds = [0.07, 0.09, 0.1, 0.15];
+
+     
     // Добавляем одно изображение
     this.cross = this.add.image(screenW / 2, screenH / 2, 'cross')
         .setOrigin(0.5)
@@ -98,26 +128,57 @@ function create() {
     };
 
     this.input.on('pointermove', pointer => {
-        const key = getHoveredImageKey(pointer);
-        if (!key) {
-            toogleZoomOut();
-            return;
-        }
+        if (this.isTransitioning) return;
+        if(stage === 'stage0' && this.cloudDragStart && pointer.isDown){
+             // Добавьте объявления переменных здесь, если они отсутствуют
+            const mapCenterX = this.cameras.main.centerX; // или другое вычисление
+            const mapCenterY = this.cameras.main.centerY;
+            const mapWidth = this.cameras.main.width;
+            const mapHeight = this.cameras.main.height;
 
-        const image = hoveredImages[Number(key.slice(1))];
+            const dx = pointer.x - this.cloudDragStart.x;
+            const dy = pointer.y - this.cloudDragStart.y;
 
-        const localX = pointer.x - (image.x - image.displayWidth / 2);
-        const localY = pointer.y - (image.y - image.displayHeight / 2);
-        const frame = this.textures.get(image.texture.key).getSourceImage();
-        const pixelX = Math.floor(localX * (frame.width / image.displayWidth));
-        const pixelY = Math.floor(localY * (frame.height / image.displayHeight));
+            const boundaryFactor = 0.5; // уменьшаем зону перемещения облаков в 2 раза
 
-        const alpha = this.textures.getPixelAlpha(pixelX, pixelY, image.texture.key);
-        if(!showStage2){
-            if (alpha > 0 ) {
-                toogleZoomIn.call(this, image);
-            } else {
-                toogleZoomOut.call(this);
+            const leftBound = mapCenterX - (mapWidth / 2) * boundaryFactor;
+            const rightBound = mapCenterX + (mapWidth / 2) * boundaryFactor;
+            const topBound = mapCenterY - (mapHeight / 2) * boundaryFactor;
+            const bottomBound = mapCenterY + (mapHeight / 2) * boundaryFactor;
+
+            for(let i = 0; i < this.cloudLayers.length; i++){
+                let newX = this.cloudInitialPositions[i].x + dx * this.cloudSpeeds[i];
+                let newY = this.cloudInitialPositions[i].y + dy * this.cloudSpeeds[i];
+
+                newX = Phaser.Math.Clamp(newX, leftBound, rightBound);
+                newY = Phaser.Math.Clamp(newY, topBound, bottomBound);
+
+                this.cloudLayers[i].x = newX;
+                this.cloudLayers[i].y = newY;
+            }
+        }else{
+            if(stage!=='stage1') return;
+            const key = getHoveredImageKey(pointer);
+            if (!key) {
+                toogleZoomOut();
+                return;
+            }
+
+            const image = hoveredImages[Number(key.slice(1))];
+
+            const localX = pointer.x - (image.x - image.displayWidth / 2);
+            const localY = pointer.y - (image.y - image.displayHeight / 2);
+            const frame = this.textures.get(image.texture.key).getSourceImage();
+            const pixelX = Math.floor(localX * (frame.width / image.displayWidth));
+            const pixelY = Math.floor(localY * (frame.height / image.displayHeight));
+
+            const alpha = this.textures.getPixelAlpha(pixelX, pixelY, image.texture.key);
+            if(!showStage2){
+                if (alpha > 0 ) {
+                    toogleZoomIn.call(this, image);
+                } else {
+                    toogleZoomOut.call(this);
+                }
             }
         }
     });
@@ -134,9 +195,29 @@ function create() {
         this.cross.setDisplaySize(originalWidth * scale, originalHeight * scale);
         this.cross.setPosition(screenW / 2, screenH / 2);
 
+        this.cloudLayers.forEach((layer, index) => {
+        layer.list.forEach(child => {
+            child.setDisplaySize(originalWidth * scale, originalHeight * scale);
+        });
+
+        // Создаем tween для плавного перемещения контейнера в центр
+        this.tweens.add({
+                targets: layer,
+                x: screenW / 2,
+                y: screenH / 2,
+                ease: 'Power2',
+                duration: 1000,
+                delay: index *50 // можно добавить небольшую задержку между слоями
+            });
+        });
+
+        this.enterToStage1.setDisplaySize(originalWidth * scale, originalHeight * scale);
+        this.enterToStage1.setPosition(screenW / 2, screenH / 2);
+
         imageKeys.forEach((key, index) => {
             hoveredImages[index + 1].setDisplaySize(originalWidth * scale, originalHeight * scale);
             hoveredImages[index + 1].setPosition(screenW / 2, screenH / 2);
+
         });
         
         initialScaleX = hoveredImages[1].scaleX;
@@ -150,39 +231,113 @@ function create() {
 
     // 2. Слушаем клик
     this.input.on('pointerdown', (pointer) => {
-        if(showStage2){
-            if (!stageThreeIsOpen && !constructorIsOpen) {
-                closeStage2();
-                showStage2 = false;
-            }
-            return;
-        }
-
-        const key = getHoveredImageKey(pointer);
-        if (!key) return;
-
-        const image = hoveredImages[Number(key.slice(1))];
-
-        const localX = pointer.x - (image.x - image.displayWidth / 2);
-        const localY = pointer.y - (image.y - image.displayHeight / 2);
-        const frame = this.textures.get(image.texture.key).getSourceImage();
-        const pixelX = Math.floor(localX * (frame.width / image.displayWidth));
-        const pixelY = Math.floor(localY * (frame.height / image.displayHeight));
-
-        const alpha = this.textures.getPixelAlpha(pixelX, pixelY, image.texture.key);
-
-        if (alpha > 0) {
-            if(!showStage2){
-                toogleZoomIn.call(this, image);
-                if(key==='a2'){
-                    showStage2 = true;
-                    openStage2();
+        if (this.isTransitioning) return;
+        if(stage==='stage1'){
+            if(showStage2){
+                if (!stageThreeIsOpen && !constructorIsOpen) {
+                    closeStage2();
+                    showStage2 = false;
                 }
+                return;
+            }
+
+            const key = getHoveredImageKey(pointer);
+            if (!key) return;
+
+            const image = hoveredImages[Number(key.slice(1))];
+
+            const localX = pointer.x - (image.x - image.displayWidth / 2);
+            const localY = pointer.y - (image.y - image.displayHeight / 2);
+            const frame = this.textures.get(image.texture.key).getSourceImage();
+            const pixelX = Math.floor(localX * (frame.width / image.displayWidth));
+            const pixelY = Math.floor(localY * (frame.height / image.displayHeight));
+
+            const alpha = this.textures.getPixelAlpha(pixelX, pixelY, image.texture.key);
+
+            if (alpha > 0) {
+                if(!showStage2){
+                    toogleZoomIn.call(this, image);
+                    if(key==='a2'){
+                        showStage2 = true;
+                        openStage2();
+                    }
+                }
+            }
+        }else{
+             // Рассчитываем локальные координаты клика внутри картинки
+            const localX = pointer.x - (this.enterToStage1.x - this.enterToStage1.displayWidth / 2);
+            const localY = pointer.y - (this.enterToStage1.y - this.enterToStage1.displayHeight / 2);
+
+            // Получаем исходное изображение для получения пикселя
+            const frame = this.textures.get(this.enterToStage1.texture.key).getSourceImage();
+
+            // Преобразуем координаты в пиксели исходного изображения
+            const pixelX = Math.floor(localX * (frame.width / this.enterToStage1.displayWidth));
+            const pixelY = Math.floor(localY * (frame.height / this.enterToStage1.displayHeight));
+
+            // Проверяем альфа-канал пикселя
+            const alpha = this.textures.getPixelAlpha(pixelX, pixelY, this.enterToStage1.texture.key);
+
+            if(alpha > 0){
+                // Запускаем анимацию облаков
+                this.isTransitioning = true;
+                diveThroughCloudsAnimation.call(this);
+            }else{
+                this.cloudDragStart = { x: pointer.x, y: pointer.y };
+                this.cloudInitialPositions = this.cloudLayers.map(layer => ({ x: layer.x, y: layer.y }));
             }
         }
     });
 
     this.input.on('pointerup', (pointer) => {
-        toogleZoomOut();
+        if (this.isTransitioning) return;
+        if(stage==='stage1'){
+            toogleZoomOut();
+        }else{
+            this.cloudDragStart = null;
+            this.cloudInitialPositions = [];
+        }
     });
 }
+
+
+function diveThroughCloudsAnimation() {
+    const duration = 1800;
+
+    const currentScale = this.cloudLayers[0].scaleX;
+    const currentTextScale = this.enterToStage1.scaleX;
+
+    this.tweens.addCounter({
+        from: currentScale,
+        to: currentScale * 2,
+        duration: duration,
+        ease: 'Power1.easeInOut',
+        onUpdate: (tween) => {
+            const progress = tween.progress;
+
+            const value = Phaser.Math.Interpolation.Linear([currentScale, currentScale * 2], progress);
+            const alpha = 1 - progress;
+
+            this.cloudLayers.forEach(layer => {
+                layer.setScale(value);
+                layer.setAlpha(alpha);
+            });
+
+            // Немного быстрее прогресс для текста
+            const textProgress = Math.min(1, progress * 1.56);
+
+            const textScale = Phaser.Math.Interpolation.Linear([currentTextScale, currentTextScale * 2], textProgress);
+            const textAlpha = 1 - textProgress;
+
+            this.enterToStage1.setScale(textScale);
+            this.enterToStage1.setAlpha(textAlpha);
+        },
+        onComplete: () => {
+            stage = 'stage1';
+            this.isTransitioning = false;
+        }
+    });
+}
+
+
+
