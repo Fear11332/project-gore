d6727764a0cf7bd6fb86d32d6dd64b2c700cb733
savva <gerasimovsawwa@gmail.com>
2025-06-06 05:18:56 +0400

    try to fix open stage2 bug v4

--- map_move_zoom/js/stage1.js
+++ map_move_zoom/js/stage1.js
@@ -173,7 +173,7 @@ function create() {
         if (this.isTransitioning) return;
         if(stage === 'stage0' && this.cloudDragStart && pointer.isDown){
              // Добавьте объявления переменных здесь, если они отсутствуют
-            const mapCenterX = this.cameras.main.centerX; // или другое вычисление
+           /* const mapCenterX = this.cameras.main.centerX; // или другое вычисление
             const mapCenterY = this.cameras.main.centerY;
             const mapWidth = this.cameras.main.width;
             const mapHeight = this.cameras.main.height;
@@ -197,7 +197,7 @@ function create() {
 
                 this.cloudLayers[i].x = newX;
                 this.cloudLayers[i].y = newY;
-            }
+            }*/
         }else{
             if(stage==='stage0') return;
             const key = getHoveredImageKey(pointer);
@@ -325,8 +325,8 @@ function create() {
                 this.isTransitioning = true;
                 diveThroughCloudsAnimation.call(this);
             }else{
-                this.cloudDragStart = { x: pointer.x, y: pointer.y };
-                this.cloudInitialPositions = this.cloudLayers.map(layer => ({ x: layer.x, y: layer.y }));
+                //this.cloudDragStart = { x: pointer.x, y: pointer.y };
+                //this.cloudInitialPositions = this.cloudLayers.map(layer => ({ x: layer.x, y: layer.y }));
             }
         }
     });
@@ -336,8 +336,8 @@ function create() {
         if(stage==='stage1'){
             toogleZoomOut();
         }else{
-            this.cloudDragStart = null;
-            this.cloudInitialPositions = [];
+            //this.cloudDragStart = null;
+            //this.cloudInitialPositions = [];
         }
     });
 }
